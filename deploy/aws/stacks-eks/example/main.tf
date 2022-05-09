c6bf148d72263bbd5dcca6a5e9897873cc5a0ff3
Dibya <65091252+dibyadhar@users.noreply.github.com>
2022-05-09 09:03:21 +0100

    refactor: add dns to core infrastructure (#4)
    Co-authored-by: Dibya Dhar <dibya.dhar@amido.com>

--- deploy/aws/stacks-eks/example/main.tf
+++ deploy/aws/stacks-eks/example/main.tf
@@ -16,6 +16,11 @@ module "amido_stacks_infra" {
   cluster_endpoint_public_access  = var.cluster_endpoint_public_access
   cluster_endpoint_private_access = var.cluster_endpoint_private_access
 
+  # Route-53 Zone Configuration
+  enable_zone =  var.enable_zone
+  public_zones = var.public_zones 
+
+
   # Provides EKS API Access to Additional IAM Users and Roles, default Admin access is provided only to the cluster creator identity
   map_roles = var.map_roles
   map_users = var.map_users
