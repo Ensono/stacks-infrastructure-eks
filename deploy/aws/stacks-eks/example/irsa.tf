f5b9fc7a39215f5ed670d1d0d4c1a7e1b8fd2b80
Dibya <65091252+dibyadhar@users.noreply.github.com>
2022-05-27 14:31:22 +0100

    feat(IRSA): create new service-accounts  for sqs,sns and dynamodb access (#7)
    Co-authored-by: Dibya Dhar <dibya.dhar@amido.com>

--- deploy/aws/stacks-eks/example/irsa.tf
+++ deploy/aws/stacks-eks/example/irsa.tf
@@ -1,23 +1,93 @@
-data "aws_iam_policy_document" "s3_policy" {
+data "aws_iam_policy_document" "stacks_dotnet" {
   statement {
+
+    sid = "S3FullAccessForStacksDotNetApp"
+
+    effect = "Allow"
+
+    actions = ["s3:*"]
+
+    resources = ["*"]
+  }
+
+  statement {
+
+    sid = "DynamoDBFullAccessForStacksDotNetApp"
+
+    effect = "Allow"
+
     actions = [
-      "s3:*",
+      "dynamodb:*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+
+    sid = "SQSFullAccessForStacksDotnetApp"
+
+    effect = "Allow"
+
+    actions = [
+      "sqs:*"
+    ]
+
+    resources = ["*"]
+  }
+  statement {
+
+    sid = "SNSFullAccessForStacksDotnetApp"
+
+    effect = "Allow"
+
+    actions = [
+      "sns:*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid = "S3FullAccessForStacksDotnetApp"
+
+    effect = "Allow"
+
+    actions = [
+      "s3:*"
     ]
 
     resources = [
-      "*",
+      "*"
     ]
   }
 }
 
-module "s3_policy_role" {
+
+module "s3_irsa" {
+
   source                = "git::https://github.com/amido/stacks-terraform//aws/modules/infrastructure_modules/eks_irsa"
-  enable_irsa           = true
-  namespace             = "default"
-  serviceaccount        = "s3-policy"
+  enable_irsa           = var.s3_irsa["create"]
+  namespace             = var.s3_irsa["namespace"]
+  serviceaccount        = var.s3_irsa["service-account-name"]
   create_serviceaccount = true
   cluster               = module.amido_stacks_infra.cluster_name
   issuer_url            = replace(module.amido_stacks_infra.cluster_oidc_issuer_url, "https://", "")
   aws_account_id        = local.account_id
-  policy                = data.aws_iam_policy_document.s3_policy.json
+  policy                = data.aws_iam_policy_document.s3.json
+}
+
+module "stacks_dotnet_irsa" {
+
+  source                = "git::https://github.com/amido/stacks-terraform//aws/modules/infrastructure_modules/eks_irsa"
+  enable_irsa           = var.stacks_dotnet_irsa["create"]
+  namespace             = var.stacks_dotnet_irsa["namespace"]
+  serviceaccount        = var.stacks_dotnet_irsa["service-account-name"]
+  create_serviceaccount = true
+  cluster               = module.amido_stacks_infra.cluster_name
+  issuer_url            = replace(module.amido_stacks_infra.cluster_oidc_issuer_url, "https://", "")
+  aws_account_id        = local.account_id
+  policy                = data.aws_iam_policy_document.stacks_dotnet.json
 }
