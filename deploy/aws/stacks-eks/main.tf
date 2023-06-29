c43bf239b68eb525b1f7bd2d731a56cae61a5816
William Ayerst <11176822+williamayerst@users.noreply.github.com>
2023-06-29 17:27:09 +0100

    removes reference to resource group

--- deploy/aws/stacks-eks/main.tf
+++ deploy/aws/stacks-eks/main.tf
@@ -7,7 +7,7 @@ module "default_label" {
   source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
   namespace   = format("%s-%s", var.name_company, var.name_project)
   environment = var.name_environment
-  name        = "${lookup(var.location_name_map, var.resource_group_location, "eu-west-2")}-${var.name_component}"
+  name        = "${lookup(var.location_name_map, var.region, "eu-west-2")}-${var.name_component}"
   delimiter   = "-"
   tags        = local.tags
 }
