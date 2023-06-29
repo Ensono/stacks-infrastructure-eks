a5b8a375caa04eda9a9e5d190454ad6e297bca47
William Ayerst <11176822+williamayerst@users.noreply.github.com>
2023-06-29 15:53:16 +0100

    Initial Commit

--- deploy/aws/stacks-eks/providers.tf
+++ deploy/aws/stacks-eks/providers.tf
@@ -44,27 +44,29 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = "~> 4.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "~> 3.5"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = "2.1.0"
+      version = "~> 2.4"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = "3.1.0"
+      version = "~> 3.2"
     }
 
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.9.0"
-    }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = ">= 2.0.1"
+    # }
   }
+
 }
+
