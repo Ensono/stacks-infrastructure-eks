5ee0aa86d010479acb9fc42f2ef9d7a78b475bad
Jack Blower <Jack@elvenspellmaker.co.uk>
2023-09-08 20:27:25 +0100

    fix: Remove k8s provider totally

--- deploy/aws/stacks-eks/providers.tf
+++ deploy/aws/stacks-eks/providers.tf
@@ -4,20 +4,6 @@
 ########################################
 provider "aws" {}
 
-provider "kubernetes" {
-
-  host                     = module.amido_stacks_infra.cluster_endpoint
-  cluster_ca_certificate   = base64decode(module.amido_stacks_infra.cluster_certificate_authority_data)
-  config_context_auth_info = "aws"
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.amido_stacks_infra.cluster_name]
-  }
-
-}
-
 provider "helm" {
   kubernetes {
     host                   = module.amido_stacks_infra.cluster_endpoint
