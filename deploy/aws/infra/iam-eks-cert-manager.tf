22cefd9adbe8e5c5ed8b20b5aef2e7a65b4b881a
Jack Blower <Jack@elvenspellmaker.co.uk>
2025-01-13 10:25:22 +0000

    feat: bumps aws provider versions

--- deploy/aws/infra/iam-eks-cert-manager.tf
+++ deploy/aws/infra/iam-eks-cert-manager.tf
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "cert_manager" {
 module "cert_manager_irsa_iam_role" {
   count = var.cert_manager_enabled ? 1 : 0
 
-  source = "git::https://github.com/Ensono/stacks-terraform//aws/modules/infrastructure_modules/eks_irsa?ref=v6.0.27"
+  source = "git::https://github.com/Ensono/stacks-terraform//aws/modules/infrastructure_modules/eks_irsa?ref=v6.0.31"
 
   cluster_name            = module.default_label.id
   cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
