217fc79847c2292d3fa32eed2fcf981165b99f44
Jack Blower <Jack@elvenspellmaker.co.uk>
2026-05-12 00:02:20 +0100

    feat: move to spot instances but t3.large

--- deploy/aws/infra/eks-cluster.tf
+++ deploy/aws/infra/eks-cluster.tf
@@ -10,6 +10,7 @@ module "eks" {
   eks_minimum_nodes                       = var.eks_minimum_nodes
   eks_maximum_nodes                       = var.eks_maximum_nodes
   eks_node_size                           = var.eks_node_size
+  eks_node_type                           = var.eks_node_type
   cluster_endpoint_public_access          = var.cluster_endpoint_public_access
   cluster_endpoint_private_access         = var.cluster_endpoint_private_access
   cluster_single_az                       = var.cluster_single_az
